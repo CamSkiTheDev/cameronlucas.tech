c0afe0dd94a0909c02c246e119af2f7123116080
CamSkiTheDev <cameronlucasaim@gmail.com>
2020-02-24 18:59:25 -0700

    made navbar mobile friendly.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,18 +5,32 @@ import logo from '../../logo.jpg'
 export default () => {
   const initState = {
     activeTab: 'Home',
+    isMobile: false,
+    isOpen: false,
   }
 
   const [state, setState] = useState(initState)
 
   return (
-    <nav>
-      <img src={logo} alt="My Logo" />
-      <ul>
+    <nav
+      style={{
+        height: state.isOpen ? 'auto' : '55px',
+      }}
+    >
+      <img
+        src={logo}
+        alt="My Logo"
+        style={{ alignSelf: state.isOpen ? 'flex-start' : 'center' }}
+      />
+      <ul style={{ flexDirection: state.isOpen ? 'column' : 'row' }}>
         <li
           style={{
             borderBottom:
               state.activeTab === 'Home' ? `solid 2px #F0DB4F` : null,
+            padding: state.isOpen ? '20px' : '0 20px',
+            width: state.isOpen ? '122px' : 'auto',
+            order: state.isOpen ? 2 : null,
+            display: state.isMobile ? (state.isOpen ? 'flex' : 'none') : null,
           }}
         >
           Home
@@ -25,6 +39,10 @@ export default () => {
           style={{
             borderBottom:
               state.activeTab === 'Portfolio' ? `solid 2px #F0DB4F` : null,
+            padding: state.isOpen ? '20px' : '0 20px',
+            width: state.isOpen ? '122px' : 'auto',
+            order: state.isOpen ? 3 : null,
+            display: state.isMobile ? (state.isOpen ? 'flex' : 'none') : null,
           }}
         >
           Portfolio
@@ -33,9 +51,21 @@ export default () => {
           style={{
             borderBottom:
               state.activeTab === 'Contact Us' ? `solid 2px #F0DB4F` : null,
+            padding: state.isOpen ? '20px' : '0 20px',
+            width: state.isOpen ? '122px' : 'auto',
+            order: state.isOpen ? 4 : null,
+            display: state.isMobile ? (state.isOpen ? 'flex' : 'none') : null,
           }}
         >
           Contact Me
+        </li>
+        <li
+          style={{ order: state.isOpen ? 1 : null, alignSelf: 'flex-end' }}
+          onClick={() =>
+            setState({ ...state, isMobile: true, isOpen: !state.isOpen })
+          }
+        >
+          <i className="fas fa-bars"></i>
         </li>
       </ul>
     </nav>
