782aaaea1b5903bcd1009a1714624212c05b39e8
CamSkiTheDev <cameronlucasaim@gmail.com>
2020-03-02 21:39:30 -0700

    added portfolio route.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from './pages/home'
+import Portfolio from './pages/portfolio'
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/portfolio" component={Portfolio} />
     <Route path="*" component={() => <h1>404 Page Not Found!</h1>} />
   </Switch>
 )
